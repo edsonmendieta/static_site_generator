f4bcc59441a066d15f4e4a65ca03dd6bcfdf8f0d
edsonmendieta <edsonj.mendieta@gmail.com>
2025-01-24 03:54:39 -0800

    a couple of helper functions w/ associated tests

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -1,19 +1,28 @@
 from typing import List
 from textnode import TextType, TextNode
 
-# def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: TextType) -> List[TextNode]:
-#     new_nodes = []
+def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: TextType) -> List[TextNode]:
+    new_nodes = []
 
-#     for node in old_nodes:
-#         if node.text_type is not TextType.NORMAL:
-#             new_nodes.append(node)
-#             continue
-#         else:
-#             if node.text.count(delimiter) % 2 is not 0: # missing a matching closing delimiter
-#                 raise Exception("Node text contains invalid Markdown syntax.")
+    for node in old_nodes:
+        if node.text_type is not TextType.NORMAL or node.text.count(delimiter) == 0:
+            new_nodes.append(node)
+            continue
+        else:
+            if node.text.count(delimiter) % 2 != 0: # missing a matching closing delimiter
+                raise Exception("Node text contains invalid Markdown syntax.")
+
+            split_strings = split_node_strings(node, delimiter)
+            for string in split_strings:
+                if delimiter in string:
+                    new_nodes.append(TextNode(string.lstrip(delimiter).rstrip(delimiter), text_type))
+                else:
+                    new_nodes.append(TextNode(string, TextType.NORMAL))
+        
+    return new_nodes
 
 
-def split_markup_strings(node: TextNode, delimiter: str) -> List[int]:
+def split_node_strings(node: TextNode, delimiter: str) -> List[int]:
     sub_strings = []
     curr_str = ""
     for i in range(0, len(node.text)): 
@@ -33,19 +42,11 @@ def split_markup_strings(node: TextNode, delimiter: str) -> List[int]:
     else:
         sub_strings.append(curr_str)
 
-    final_strings = [] # Remove empty strings from return value
+    split_strings = [] # Remove empty strings from return value
     for sub in sub_strings:
-        if sub:    
-            final_strings.append(sub)
+        if delimiter in sub and len(sub) > len(delimiter) * 2:
+            split_strings.append(sub)
+        if sub and delimiter not in sub:
+            split_strings.append(sub)
 
-    return final_strings
-
-
-node1 = TextNode("Hello **my** name is **baseball**, ice.", TextType.NORMAL)
-node2 = TextNode("some words. more words **more words**", TextType.NORMAL)
-node3 = TextNode("**bold words** more words more words", TextType.NORMAL)
-node4 = TextNode("**bold words****bold too** more words more words", TextType.NORMAL)
-print(split_markup_strings(node1, "**"))
-print(split_markup_strings(node2, "**"))
-print(split_markup_strings(node3, "**"))
-print(split_markup_strings(node4, "**"))
+    return split_strings
