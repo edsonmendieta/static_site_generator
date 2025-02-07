265524c9feb0096ca050543eea2b62856e6d59ea
edsonmendieta <edsonj.mendieta@gmail.com>
2025-02-07 14:06:12 -0800

    added bool functions to verify if markdown block is a list

--- src/block_funcs.py
+++ src/block_funcs.py
@@ -17,3 +17,73 @@ def markdown_to_blocks(markdown: str) -> List[str]:
     
     return blocks_non_empty
 
+
+def block_to_block_type(block: str) -> str:
+    first_char = block[0]
+    block_lines = block.split('\n')
+
+    match first_char:
+        case '#':
+            heading = block_lines[0].split(" ")[0]
+            if len(heading) <= 6:
+                is_heading = True
+                for char in heading:
+                    if char != '#':
+                        is_heading = False
+                if is_heading:
+                    return "heading"
+        case '`':
+            if block[:3] == "```" and block[-3:] == "```":
+                return "code"
+        case '>':
+            is_quote_block = True
+            for line in block_lines:
+                if line[0] != '>':
+                    is_quote_block = False
+                    break
+            if is_quote_block:
+                return "quote"
+        case '*' | '-':
+            is_ul = True
+            for line in block_lines:
+                if line[0] + line[1] != "* " or line[0] + line[1] != "- ":
+                    is_ul = False
+                    break
+            if is_ul:
+                return "unordered_list"
+    
+    if is_ordered_list(block):
+        return "ordered_list"
+
+def is_unordered_list(block:str) -> bool:
+    block_lines = block.split('\n')
+
+    for line in block_lines:
+        if line[0] + line[1] != "* " and line[0] + line[1] != "- ":
+            return False
+    
+    return True
+
+def is_ordered_list(block: str) -> bool:
+    block_lines = block.split('\n')
+    
+    line_numbers = []
+    for line in block_lines:
+        period_split = line.split('.')
+        if period_split[0].isdigit() == False or period_split[1][0] != " ":
+            return False
+        else:
+            line_numbers.append(period_split[0])
+    
+    if line_numbers[0] != "1":
+        print(line_numbers[0])
+        return False 
+    else:
+        prev_line_number = -1
+        for number in line_numbers:
+            if int(number) <= int(prev_line_number):
+                return False
+            else:
+                prev_line_number = number
+
+    return True
--- src/tests/test_block_funcs.py
+++ src/tests/test_block_funcs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.block_funcs import markdown_to_blocks
+from src.block_funcs import markdown_to_blocks, is_unordered_list, is_ordered_list
 
 
 class TestBlockFuncs(unittest.TestCase):
@@ -55,6 +55,30 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                 '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
             ]
         )
+    
+    def test_is_unordered_list(self):
+        test_str = """* wlfhh a;lkfj  a;fj\n- aflk;jaf;lkjaf ;lakf \n* aflkjafjl alfj"""
+        test_str2 = """*wlfhh a;lkfj  a;fj\n- aflk;jaf;lkjaf ;lakf \n* aflkjafjl alfj"""
+        test_str3 = """ * wlfhh a;lkfj  a;fj\n- aflk;jaf;lkjaf ;lakf \n* aflkjafjl alfj"""
+        test_str4 = """* wlfhh a;lkfj  a;fj\n* aflk;jaf;lkjaf ;lakf \n* aflkjafjl alfj"""
+        test_str5 = """- wlfhh a;lkfj  a;fj\n- aflk;jaf;lkjaf ;lakf \n- aflkjafjl alfj"""
+
+        self.assertTrue(is_unordered_list(test_str))
+        self.assertFalse(is_unordered_list(test_str2))
+        self.assertFalse(is_unordered_list(test_str3))
+        self.assertTrue(is_unordered_list(test_str4))
+        self.assertTrue(is_unordered_list(test_str5))
+
+    def test_is_ordered_list(self):
+        test_str = """1. asdl;fkjasdf\n2.asl;dfkja;sdf as;dja\n3.lkj; alskjf lkajf;"""
+        test_str2 = """1. asdl;fkjasdf\n2. asl;dfkja;sdf as;dja\n3. lkj; alskjf lkajf;"""
+        test_str3 = """1. asdl;fkjasdf\n 2. asl;dfkja;sdf as;dja\n3. lkj; alskjf lkajf;"""
+        test_str4 = """1. asdl;fkjasdf\n2 . asl;dfkja;sdf as;dja\n3. lkj; alskjf lkajf;"""
+
+        self.assertFalse(is_ordered_list(test_str))
+        self.assertTrue(is_ordered_list(test_str2))
+        self.assertFalse(is_ordered_list(test_str3))
+        self.assertFalse(is_ordered_list(test_str4))
 
 
 if __name__ == "_main_":
